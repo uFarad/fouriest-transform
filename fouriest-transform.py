a6572f4058d8a478e96013e9b94fdbe4e7df3aad
uFarad <root@ufarad.com>
2024-03-10 22:54:00 -0400

    Added comments

--- fouriest-transform.py
+++ fouriest-transform.py
@@ -1,16 +1,20 @@
 import argparse
 
+#Constants for min and max radix
 RADIX_MIN = 4
 RADIX_MAX = 16
 
+#Argument handling
 parser = argparse.ArgumentParser("Transforms a number to a radix in which is has the most fours.")
 parser.add_argument("number", type=int, help="Starting number")
 parser.add_argument("radix", type=int, help="Starting radix/base")
 
 args = parser.parse_args()
 
+#Convert starting number to base 10 int
 number_start = int(str(args.number), args.radix)
 
+#Iterate through all valid radices
 for radix in range(RADIX_MIN, RADIX_MAX + 1):
 	number = number_start
 	
